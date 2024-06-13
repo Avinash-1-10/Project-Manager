fb761a46953b25b1b56b5bb9a1bb5fd20681b4ed
Avinash-1-10 <avinashyede110@gmail.com>
2024-06-13 23:37:27 +0530

    add project model

---
+++ server/models/ProjectModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const projectSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    dueDate: {
+      type: Date,
+      required: true,
+    },
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Project = mongoose.model("Project", projectSchema);
+export default Project;
