bafa380902cd126f382bc8a783241740b2ada191
Avinash-1-10 <avinashyede110@gmail.com>
2024-06-20 23:39:53 +0530

    integrate register api with ui

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -4,15 +4,25 @@ import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai"; // Eye icons
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import { useNavigate } from "react-router-dom";
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+import axios from "axios";
 
 // Validation schema
 const schema = yup.object().shape({
   firstName: yup.string().required("First Name is required"),
   lastName: yup.string().required("Last Name is required"),
+  email: yup
+    .string()
+    .required("Email is required")
+    .email("Must be a valid email"),
   username: yup
     .string()
     .required("Username is required")
-    .matches(/^[a-zA-Z0-9_]{3,20}$/, "Username must be 3-20 characters and contain only letters, numbers, and underscores"),
+    .matches(
+      /^[a-zA-Z0-9_]{3,20}$/,
+      "Username must be 3-20 characters and contain only letters, numbers, and underscores"
+    ),
   password: yup
     .string()
     .required("Password is required")
@@ -20,15 +30,21 @@ const schema = yup.object().shape({
   confirmPassword: yup
     .string()
     .required("Confirm Password is required")
-    .oneOf([yup.ref('password')], "Passwords must match"),
+    .oneOf([yup.ref("password")], "Passwords must match"),
 });
 
 const Register = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-  const { register, handleSubmit, formState: { errors } } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     resolver: yupResolver(schema),
   });
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const togglePasswordVisibility = () => {
     setShowPassword((prevState) => !prevState);
@@ -39,8 +55,18 @@ const Register = () => {
   };
 
   const onSubmit = async (data) => {
-    console.log("Form Data: ", data);
-    // Handle form submission
+    setLoading(true);
+    try {
+      const response = await axios.post(`${backendUrl}/users`, data);
+      if (response.status === 201) {
+        navigate("/login");
+      }
+    } catch (error) {
+      console.error("Registration failed:", error);
+      // Handle error (e.g., show a notification to the user)
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -59,7 +85,9 @@ const Register = () => {
                 className="input input-bordered w-full"
                 {...register("firstName")}
               />
-              {errors.firstName && <p className="text-red-600">{errors.firstName.message}</p>}
+              {errors.firstName && (
+                <p className="text-red-600">{errors.firstName.message}</p>
+              )}
             </div>
             <div className="form-control w-1/2">
               <label className="label">
@@ -71,8 +99,24 @@ const Register = () => {
                 className="input input-bordered w-full"
                 {...register("lastName")}
               />
-              {errors.lastName && <p className="text-red-600">{errors.lastName.message}</p>}
+              {errors.lastName && (
+                <p className="text-red-600">{errors.lastName.message}</p>
+              )}
             </div>
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="Email"
+              className="input input-bordered w-full"
+              {...register("email")}
+            />
+            {errors.email && (
+              <p className="text-red-600">{errors.email.message}</p>
+            )}
           </div>
           <div className="form-control">
             <label className="label">
@@ -84,7 +128,9 @@ const Register = () => {
               className="input input-bordered w-full"
               {...register("username")}
             />
-            {errors.username && <p className="text-red-600">{errors.username.message}</p>}
+            {errors.username && (
+              <p className="text-red-600">{errors.username.message}</p>
+            )}
           </div>
           <div className="form-control relative">
             <label className="label">
@@ -106,7 +152,9 @@ const Register = () => {
                 <AiFillEye size={24} />
               )}
             </div>
-            {errors.password && <p className="text-red-600">{errors.password.message}</p>}
+            {errors.password && (
+              <p className="text-red-600">{errors.password.message}</p>
+            )}
           </div>
           <div className="form-control relative">
             <label className="label">
@@ -128,11 +176,21 @@ const Register = () => {
                 <AiFillEye size={24} />
               )}
             </div>
-            {errors.confirmPassword && <p className="text-red-600">{errors.confirmPassword.message}</p>}
+            {errors.confirmPassword && (
+              <p className="text-red-600">{errors.confirmPassword.message}</p>
+            )}
           </div>
           <div className="form-control">
-            <button type="submit" className="btn btn-primary w-full">
-              Register
+            <button
+              type="submit"
+              className="btn btn-primary w-full"
+              disabled={loading}
+            >
+              {loading ? (
+                <span className="loading loading-infinity loading-lg"></span>
+              ) : (
+                "Register"
+              )}
             </button>
           </div>
         </form>
