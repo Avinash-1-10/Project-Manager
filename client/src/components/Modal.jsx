138a9f9d14042c62c449abc65fd1729da91d93ad
Avinash-1-10 <avinashyede110@gmail.com>
2024-07-05 17:09:57 +0530

    modal bug fix

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,19 +1,26 @@
+import { IoMdClose } from "react-icons/io";
+
 const Modal = ({ isVisible, hideModal, children }) => {
-    if (!isVisible) return null;
-  
-    return (
-      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-        <div className="bg-gray-900 border border-gray-700 p-6 rounded shadow-lg w-96">
-          <div className="">
-            {children}
-          </div>
-          <div className=" mt-4">
-            <button onClick={hideModal} className="btn btn-primary">Close</button>
-          </div>
+  if (!isVisible) return null;
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="bg-gray-900 border border-gray-700 p-6 rounded shadow-lg relative">
+        <div
+          className="absolute top-1 right-1 p-2 text-white text-lg cursor-pointer rounded-full hover:bg-gray-800 transition-all duration-300"
+          onClick={hideModal}
+        >
+          <IoMdClose />
+        </div>
+        <div className="">{children}</div>
+        <div className=" mt-4">
+          <button onClick={hideModal} className="btn btn-primary">
+            Close
+          </button>
         </div>
       </div>
-    );
-  };
-  
-  export default Modal;
-  
+    </div>
+  );
+};
+
+export default Modal;
